ff940696ec0d9cc262de77c6aed34f2ff48e77b8
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-05-13 17:37:59 +0530

    added alphabet div

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 import useToken from "./utils/useToken";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppContainer from "./components/AppContainer/AppContainer";
+import Alphabet from "./components/AppContainer/Alphabet";
 import wall from "./assets/background.jpg"; // Do not remove. Needed to import background as prod asset
 import LoginRegistrationPage from "./view/LoginRegister";
 
 function App() {
-  const { token, setToken } = useToken();
-  const dummy = wall;
-  if(!token) { // changeit - remove false
-    return <LoginRegistrationPage setToken={setToken} />
-  }
+  // const { token, setToken } = useToken();
+  // const dummy = wall;
+  // if(!token) { // changeit - remove false
+  //   return <LoginRegistrationPage setToken={setToken} />
+  // }
   return (
     <React.Fragment>
       <Router>
-        <AppContainer />
+        <Alphabet />
       </Router>
     </React.Fragment>
   );
